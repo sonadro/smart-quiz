f814395d73e4961e60a4b8f262a05a3a4a756d5c
sonadro <112541290+sonadro@users.noreply.github.com>
2022-09-28 17:13:16 +0200

    Files upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const correctAnsers = ["B", "B", "B", "A"];
+const form = document.querySelector("quiz-form");
+
+console.log(form);
+
+// form.addEventListener("submit", e => {
+//     e.preventDefault();
+// });
