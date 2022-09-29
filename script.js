71e4cf700d9877394a35bccc934354bdc2fd1a13
sonadro <sondre.rooth@gmail.com>
2022-09-29 09:06:46 +0200

    Finished quiz

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-const correctAnsers = ["B", "B", "B", "A"];
-const form = document.querySelector("quiz-form");
+// Assigning variables
+const correctAnswers = ["B", "B", "B", "A"];
+const form = document.querySelector(".quiz-form");
+const result = document.querySelector("div.result");
 
-console.log(form);
+// On submit:
+form.addEventListener("submit", e => {
+    e.preventDefault(); // Prevent default action
 
-// form.addEventListener("submit", e => {
-//     e.preventDefault();
-// });
+    // Get user answers
+    let userAnswers = [];
+    for (i = 1; i < 5; i++) {
+        userAnswers.push(eval(`form.q${i}.value`));
+    }
+    
+    // Check answers
+    let score = 0;
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+
+    // Show and animate score
+    scrollTo(0, 0);
+    const resultValue = result.querySelector("span");
+    let interval = 0;
+    const showScore = setInterval(() => {
+        resultValue.textContent = `${interval}%`;
+        if (interval >= score) {
+            clearInterval(showScore);
+        } else {
+            interval++;
+        }
+    }, 10);
+    result.classList.remove("d-none");
+});
